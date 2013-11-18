687b71a8a47c3a12e69a541f1b904a141d3a4b6a
Cory Bohon <cory@corybohon.com>
2013-11-18 12:47:58 -0500

    Initial Commit.

---
+++ OS X Testing Mindmap.mm
@@ -0,0 +1,211 @@
+<map version="0.9.0">
+    <node TEXT="OS X Testing">
+        <node TEXT="Software">
+            <node TEXT="OS X">
+                <node TEXT="10.6.x"></node>
+                <node TEXT="10.7.x"></node>
+                <node TEXT="10.8.x"></node>
+                <node TEXT="10.9.x"></node>
+            </node>
+            <node TEXT="In-App Purchase">
+                <node TEXT="Allows Purchase"></node>
+                <node TEXT="Restores Purchases"></node>
+            </node>
+            <node TEXT="Crash Reporting"></node>
+            <node TEXT="Analytics"></node>
+            <node TEXT="Gestures"></node>
+            <node TEXT="Memory Warnings"></node>
+            <node TEXT="Data Saved Before Exit">
+                <node TEXT="Forced Quit"></node>
+                <node TEXT="Standard Exit"></node>
+            </node>
+            <node TEXT="Trial">
+                <node TEXT="Usage available through trial-end date"></node>
+                <node TEXT="Trial ends properly">
+                    <node TEXT="Allows no/limited functionality"></node>
+                </node>
+                <node TEXT="Provides action"></node>
+                <node TEXT="Switching system time ends trial">
+                    <node TEXT="Setting Date Ahead of End-Date"></node>
+                    <node TEXT="Setting Date Behind Beginnign Date"></node>
+                </node>
+            </node>
+            <node TEXT="Sandboxed"></node>
+        </node>
+        <node TEXT="Network">
+            <node TEXT="Ethernet"></node>
+            <node TEXT="Ethernet (USB Adapter)"></node>
+            <node TEXT="Wi-Fi"></node>
+            <node TEXT="Tethering">
+                <node TEXT="Bluetooth Tethering"></node>
+                <node TEXT="Wi-Fi Tethering"></node>
+                <node TEXT="USB Tethering"></node>
+            </node>
+            <node TEXT="Network Link Conditioning Test">
+                <node TEXT="Error Messages"></node>
+                <node TEXT="Non-blocking"></node>
+                <node TEXT="First Launch Message"></node>
+            </node>
+        </node>
+        <node TEXT="Audio/Video">
+            <node TEXT="AirPlay">
+                <node TEXT="AirPlay Audio"></node>
+                <node TEXT="AirPlay video"></node>
+            </node>
+            <node TEXT="Multiple Displays">
+                <node TEXT="Mission Control"></node>
+                <node TEXT="Exposé"></node>
+            </node>
+            <node TEXT="Full Screen Support"></node>
+            <node TEXT="Ports">
+                <node TEXT="Line In"></node>
+                <node TEXT="Line out"></node>
+            </node>
+            <node TEXT="Other">
+                <node TEXT="Bluetooth Audio"></node>
+                <node TEXT="USB Audio"></node>
+            </node>
+            <node TEXT="Independent Audio Controls"></node>
+        </node>
+        <node TEXT="Functionality">
+            <node TEXT="Communication">
+                <node TEXT="Email">
+                    <node TEXT="Configured"></node>
+                    <node TEXT="Not Configured"></node>
+                </node>
+                <node TEXT="Twitter">
+                    <node TEXT="Configured"></node>
+                    <node TEXT="Not Configured"></node>
+                </node>
+                <node TEXT="iMessage">
+                    <node TEXT="Configured"></node>
+                    <node TEXT="Not Configured"></node>
+                </node>
+                <node TEXT="AirDrop"></node>
+                <node TEXT="Facebook">
+                    <node TEXT="Not Configured"></node>
+                    <node TEXT="Configured"></node>
+                </node>
+                <node TEXT="Flickr">
+                    <node TEXT="Not Configured"></node>
+                    <node TEXT="Configured"></node>
+                </node>
+            </node>
+            <node TEXT="Updates">
+                <node TEXT="MAS"></node>
+                <node TEXT="Sparkle"></node>
+            </node>
+            <node TEXT="Web Views">
+                <node TEXT="Drag and Drop"></node>
+                <node TEXT="Full Navigation"></node>
+            </node>
+            <node TEXT="Table Views">
+                <node TEXT="Allows Reordering"></node>
+                <node TEXT="Allows Sorting"></node>
+            </node>
+            <node TEXT="Secure Info">
+                <node TEXT="Keychain Access"></node>
+            </node>
+            <node TEXT="Save">
+                <node TEXT="Save Before Exit"></node>
+                <node TEXT="Undo/Redo"></node>
+            </node>
+            <node TEXT="iCloud Data">
+                <node TEXT="Documents">
+                    <node TEXT="Folders"></node>
+                    <node TEXT="Save"></node>
+                    <node TEXT="Delete"></node>
+                </node>
+                <node TEXT="UserDefaults">
+                    <node TEXT="Synced"></node>
+                    <node TEXT="Doesn't Overwrite"></node>
+                </node>
+                <node TEXT="Core Data">
+                    <node TEXT="Syncs"></node>
+                    <node TEXT="Doesn't Overwrite"></node>
+                </node>
+                <node TEXT="Configured / Not Configured"></node>
+            </node>
+            <node TEXT="Standard Shortcuts">
+                <node TEXT="Select All"></node>
+                <node TEXT="Cut / Copy / Paste"></node>
+            </node>
+            <node TEXT="Privacy">
+                <node TEXT="Location Services"></node>
+                <node TEXT="Contacts"></node>
+                <node TEXT="Calendars"></node>
+                <node TEXT="Reminders"></node>
+                <node TEXT="Accessibility"></node>
+            </node>
+        </node>
+        <node TEXT="UI">
+            <node TEXT="Smooth Animations">
+                <node TEXT="Non-blocking"></node>
+            </node>
+            <node TEXT="Accessibility">
+                <node TEXT="VoiceOver"></node>
+                <node TEXT="Screen Magnification"></node>
+                <node TEXT="Keyboard Button Access"></node>
+                <node TEXT="Accessibility Labels"></node>
+                <node TEXT="Menubar app"></node>
+                <node TEXT="Other Events">
+                    <node TEXT="Gestures"></node>
+                    <node TEXT="Keyboard Shortcuts"></node>
+                </node>
+            </node>
+            <node TEXT="Menubar app">
+                <node TEXT="Exits"></node>
+                <node TEXT="Non-blocking"></node>
+                <node TEXT="Provides Utility"></node>
+            </node>
+            <node TEXT="Notification Center">
+                <node TEXT="Clears notifications"></node>
+                <node TEXT="Overuse of Notifications"></node>
+            </node>
+            <node TEXT="Window">
+                <node TEXT="Resize"></node>
+                <node TEXT="Close"></node>
+                <node TEXT="Minimize"></node>
+                <node TEXT="Maximize"></node>
+                <node TEXT="Full-screen"></node>
+            </node>
+            <node TEXT="Dock">
+                <node TEXT="Click to show window"></node>
+                <node TEXT="Icon Visibilty"></node>
+            </node>
+            <node TEXT="Scrollbars"></node>
+        </node>
+        <node TEXT="Hardware">
+            <node TEXT="Notebook">
+                <node TEXT="MacBook Air"></node>
+                <node TEXT="MacBook Pro (Retina)"></node>
+                <node TEXT="MacBook Pro (Hi-Res)"></node>
+                <node TEXT="MacBook Pro"></node>
+            </node>
+            <node TEXT="Desktop">
+                <node TEXT="Mac Pro"></node>
+                <node TEXT="iMac"></node>
+                <node TEXT="Mac mini"></node>
+            </node>
+        </node>
+        <node TEXT="Data">
+            <node TEXT="Preserved through data model changes"></node>
+            <node TEXT="Time Settings">
+                <node TEXT="Properly display all time zones"></node>
+                <node TEXT="Switching between time zones"></node>
+                <node TEXT="Relative time displays"></node>
+                <node TEXT="System time too fast/slow"></node>
+                <node TEXT="24 hour clock"></node>
+            </node>
+            <node TEXT="Internationalization">
+                <node TEXT="Long translated strings fit"></node>
+                <node TEXT="Supported Languages?"></node>
+                <node TEXT="Localization text in images"></node>
+                <node TEXT="Region Formats"></node>
+            </node>
+            <node TEXT="Input">
+                <node TEXT="Dictation"></node>
+            </node>
+        </node>
+    </node>
+</map>
